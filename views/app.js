145f5a1f6a7748ba0901733bdd062c73785c1e74
Joshua <105324950+JoshKeesee@users.noreply.github.com>
2023-01-23 19:26:36 +0000

    .

--- views/app.js
+++ views/app.js
@@ -1,4 +1,5 @@
 var currSong;
+var background = "limegreen";
 
 document.querySelector("#song-input").addEventListener("change", (event) => {
   const song = event.target.files[0];
@@ -13,20 +14,67 @@ document.querySelector("#song-input").addEventListener("change", (event) => {
           base64String += String.fromCharCode(data[i]);
         }
         document.querySelector(".song-img").src = `data:${format};base64,${window.btoa(base64String)}`;
+        const rgb = getAverageRGB(document.querySelector(".song-img"));
+        document.querySelector(".container").style.background = 'rgb('+rgb.r+','+rgb.g+','+rgb.b+')';
         document.querySelector(".song-title").innerText = tag.tags.title;
         document.querySelector(".song-artist").innerText = tag.tags.artist;
         }
     });
 });
 
-function play() {
+function playSong() {
   currSong.play();
   document.querySelector(".play").classList.add("hidden");
   document.querySelector(".pause").classList.remove("hidden");
 }
 
-function pause() {
+function pauseSong() {
   currSong.pause();
   document.querySelector(".play").classList.remove("hidden");
   document.querySelector(".pause").classList.add("hidden");
+}
+
+function getAverageRGB(imgEl) {
+
+    var blockSize = 5, // only visit every 5 pixels
+        defaultRGB = {r:0,g:0,b:0}, // for non-supporting envs
+        canvas = document.createElement('canvas'),
+        context = canvas.getContext && canvas.getContext('2d'),
+        data, width, height,
+        i = -4,
+        length,
+        rgb = {r:0,g:0,b:0},
+        count = 0;
+
+    if (!context) {
+        return defaultRGB;
+    }
+
+    height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
+    width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
+
+    context.drawImage(imgEl, 0, 0);
+
+    try {
+        data = context.getImageData(0, 0, width, height);
+    } catch(e) {
+        /* security error, img on diff domain */
+        return defaultRGB;
+    }
+
+    length = data.data.length;
+
+    while ( (i += blockSize * 4) < length ) {
+        ++count;
+        rgb.r += data.data[i];
+        rgb.g += data.data[i+1];
+        rgb.b += data.data[i+2];
+    }
+
+    // ~~ used to floor values
+    rgb.r = ~~(rgb.r/count);
+    rgb.g = ~~(rgb.g/count);
+    rgb.b = ~~(rgb.b/count);
+
+    return rgb;
 }
